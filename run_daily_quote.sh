1bdabe8a6ca3467e2618049531be6a23bad83854
jrueppel <rueppel@gmail.com>
2025-06-08 21:59:02 -0700

    Add quote for 2025-06-08

--- run_daily_quote.sh
+++ run_daily_quote.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 echo "Script started at $(date)" >> /Users/jeffrueppel/Documents/GitHub/DailyQuoteLocalizationRepo/logs/script_debug.log
 
+echo ">>> run_daily_quote.sh reached top of script at $(date)" >> /Users/jeffrueppel/Documents/GitHub/DailyQuoteLocalizationRepo/logs/cron_debug.log
+
+
 # >>> Conda initialize >>>
 source /Users/jeffrueppel/miniconda3/etc/profile.d/conda.sh
 conda activate base
